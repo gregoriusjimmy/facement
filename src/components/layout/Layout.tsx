f1f1c5a88c0b38e46ae3a7bc65f8ec4458e577be
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-04-09 10:47:35 +0700

    add navbar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,25 @@
 import * as React from 'react';
 
+import ButtonLink from '../links/ButtonLink';
+import UnstyledLink from '../links/UnstyledLink';
+
+import Logo from '~/svg/facement-logo.svg';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
-  return <>{children}</>;
+  return (
+    <div>
+      <div className='bg-primary-500'>
+        <nav className='layout flex h-20 items-center justify-between py-4'>
+          <UnstyledLink href='/'>
+            <Logo className='h-11 text-9xl' />
+          </UnstyledLink>
+          <ButtonLink href='/signin' variant='outline'>
+            Sign in
+          </ButtonLink>
+        </nav>
+      </div>
+      {children}
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Layout from '@/components/layout/Layout';
 import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 
-import Logo from '~/svg/facement-logo.svg';
 import ManStandOutSVG from '~/svg/man-stand-out.svg';
 import Work1SVG from '~/svg/work1.svg';
 import Work2SVG from '~/svg/work2.svg';
@@ -14,15 +13,9 @@ import Work3SVG from '~/svg/work3.svg';
 export default function HomePage() {
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
       <main>
-        <div className='flex h-screen flex-col bg-primary-500'>
-          <nav className='layout flex h-16 items-center justify-between py-4 '>
-            <Logo className='h-11 text-9xl' />
-            <Button variant='outline'>Sign in</Button>
-          </nav>
+        <div className='flex h-[92vh] flex-col bg-primary-500'>
           <section className='flex h-full items-center'>
             <div className='layout flex flex-col space-y-5 md:flex-row-reverse md:items-center md:justify-between'>
               <NextImage
@@ -32,7 +25,7 @@ export default function HomePage() {
                 objectFit='contain'
               />
               <div className='space-y-4 text-center md:w-2/5 md:space-y-8 md:text-left'>
-                <h1 className='font-bold text-white lg:text-5xl'>
+                <h1 className='font-bold text-white lg:text-5xl lg:leading-[50px]'>
                   Easy face scan to pay your needs.
                 </h1>
                 <h3 className='font-secondary text-white'>
@@ -46,10 +39,10 @@ export default function HomePage() {
             </div>
           </section>
         </div>
-        <section className='relative overflow-hidden bg-[#F1F3FA] py-[100px] sm:py-[200px]'>
+        <section className='relative overflow-hidden bg-[#F1F3FA] py-[100px] md:py-[200px]'>
           <div className='layout z-10 space-y-5 lg:space-y-8 '>
             <h1 className='font-semibold lg:text-5xl'>Try Our Demo</h1>
-            <p className='max-w-xs sm:max-w-sm'>
+            <p className='max-w-xs md:max-w-sm'>
               Be the first to try our e-wallet account with face payment demo by
               creating your account
             </p>
@@ -65,7 +58,7 @@ export default function HomePage() {
             <h1 className='text-center sm:mb-14 md:mb-24 lg:mb-28'>
               How Facement Works
             </h1>
-            <div className='md:jus flex flex-col space-y-5 md:flex-row md:items-center'>
+            <div className='flex flex-col space-y-5 md:flex-row md:items-center'>
               <div className='md:w-1/2'>
                 <Work1SVG className='h-[200px] w-full md:h-[300px]' />
               </div>
