762d8748239ee19e96be89cec8c02962ee3dabb5
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-04-25 20:17:48 +0700

    modify InputField to have error obj

--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import clsxm from '@/lib/clsxm';
-
 type Props = {
   label: string;
   labelClassName?: string;
   inputClassName?: string;
+  error?: { isError: boolean; message: string };
 } & React.ComponentPropsWithRef<'input'>;
 
 const InputField = React.forwardRef<HTMLInputElement, Props>(
-  ({ label, className, labelClassName, inputClassName, id, ...rest }, ref) => {
+  (
+    { label, className, labelClassName, inputClassName, error, id, ...rest },
+    ref
+  ) => {
     return (
       <div className={clsxm('w-full', className)}>
         <label className={clsxm('mb-1 block', labelClassName)} htmlFor={id}>
@@ -24,6 +27,9 @@ const InputField = React.forwardRef<HTMLInputElement, Props>(
           )}
           {...rest}
         />
+        <span className='text-sm text-red-500'>
+          {error?.isError ? error?.message : ''}
+        </span>
       </div>
     );
   }
