de851a1fc9fb6e99f2f2c82a8ac640c4c5595715
gregoriusjimmy <gregoriusjimmy@gmail.com>
2022-05-11 20:22:29 +0700

    finalize ign in page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,15 +1,101 @@
-import { useMediaQuery } from '@/hooks/useMediaQuery';
+import { useRouter } from 'next/router'
+import { FormEvent, useEffect, useState } from 'react'
 
-import Button from '@/components/buttons/Button';
-import InputField from '@/components/input-field/InputField';
-import Layout from '@/components/layout/Layout';
-import PrimaryLink from '@/components/links/PrimaryLink';
-import Seo from '@/components/Seo';
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import { usePost } from '@/hooks/usePost'
 
-import SignInSVG from '~/svg/signin.svg';
+import Button from '@/components/buttons/Button'
+import InputField from '@/components/input-field/InputField'
+import Layout from '@/components/layout/Layout'
+import PrimaryLink from '@/components/links/PrimaryLink'
+import Seo from '@/components/Seo'
+
+import {
+  IAccountExistRes,
+  IAccountExistSpec,
+  IAuthLoginRes,
+  IAuthLoginSpec,
+  ITokenVerifyRes as IVerifyTokenRes,
+} from '@/types/networkTypes'
+
+import SignInSVG from '~/svg/signin.svg'
+
+type TKeyFormData = 'email' | 'password'
+
+type TFormError = {
+  [k in TKeyFormData]: { isError: boolean; message: string }
+}
+
+const formErrorDefault = {
+  email: { isError: false, message: '' },
+  password: { isError: false, message: '' },
+}
 
 export default function SignInPage() {
-  const isMd = useMediaQuery('(max-width: 768px)');
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [formError, setFormError] = useState<TFormError>(formErrorDefault)
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false)
+
+  const postAuthLogin = usePost<IAuthLoginRes, IAuthLoginSpec>('/auth/login')
+  const postAccountExist = usePost<IAccountExistRes, IAccountExistSpec>(
+    'accounts/exist'
+  )
+  const postIsTokenVerify = usePost<IVerifyTokenRes, null>('auth/verify/token')
+
+  const isMd = useMediaQuery('(max-width: 768px)')
+  const router = useRouter()
+
+  const setFormDataToError = (key: TKeyFormData, message = '') => {
+    setFormError({ ...formError, [key]: { isError: true, message } })
+  }
+
+  useEffect(() => {
+    const fetchVerifyToken = async () => {
+      try {
+        const res = await postIsTokenVerify(null)
+        if (res.ok && res.isVerified) router.push('/account')
+      } catch (error) {
+        console.error(error instanceof Error ? error.message : error)
+      }
+    }
+    fetchVerifyToken()
+  }, [postIsTokenVerify, router])
+
+  const isFormDataValid = async () => {
+    try {
+      const res = await postAccountExist({
+        email,
+      })
+      if (!res.isAccountExist) {
+        setFormDataToError('email', res.message)
+        return false
+      }
+    } catch (error) {
+      console.error(error instanceof Error ? error.message : error)
+      setFormDataToError('email', 'Server error, please try again later')
+      return false
+    }
+
+    setFormError(formErrorDefault)
+    return true
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (await isFormDataValid()) {
+      try {
+        setIsLoadingSubmit(true)
+        const res = await postAuthLogin({ email, password })
+        if (res.ok) router.push('/account')
+      } catch (error) {
+        console.error(error instanceof Error ? error.message : error)
+      } finally {
+        setIsLoadingSubmit(false)
+      }
+    }
+  }
 
   return (
     <Layout>
@@ -19,7 +105,7 @@ export default function SignInPage() {
           className='layout min-h-main flex flex-col items-center 
       justify-center md:flex-row md:justify-start '
         >
-          <form className='card w-full'>
+          <form className='card w-full' onSubmit={(e) => handleSubmit(e)}>
             <h3 className='mb-8 text-center md:mb-10'>
               Sign in into your account
             </h3>
@@ -27,16 +113,29 @@ export default function SignInPage() {
               <InputField
                 label='Email'
                 type='email'
-                placeholder='email@mail.com'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={formError.email}
+                required
               />
               <InputField
                 label='Password'
                 type='password'
-                placeholder='*********'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={formError.password}
+                required
+                minLength={6}
               />
             </div>
             <div className='flex flex-col items-center space-y-4'>
-              <Button className=' w-full max-w-sm'>Sign in</Button>
+              <Button
+                className=' w-full max-w-sm'
+                type='submit'
+                isLoading={isLoadingSubmit}
+              >
+                Sign in
+              </Button>
               <p className='text-center'>
                 Don&apos;t have an account?{' '}
                 <PrimaryLink href='/signup'>Create an account</PrimaryLink>
@@ -49,5 +148,5 @@ export default function SignInPage() {
         )}
       </main>
     </Layout>
-  );
+  )
 }
